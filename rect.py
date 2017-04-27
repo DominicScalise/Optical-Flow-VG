969d6775d1dae856bfcdbb4046e1aae3e9cbf79c
John <jpm6zv@virginia.edu>
2017-04-27 16:20:06 -0400

    paddles

---
+++ rect.py
@@ -0,0 +1,21 @@
+
+
+class paddle():
+
+    def __init__(self, center, h, w):
+        self.center = center
+        self.topleft[0] = center[0] - h/2
+        self.topleft[1] = center[1] - w/2
+        self.bottomright[0] = center[0] + h/2
+        self.bottomright[1] = center[1] + w/2
+        self.w = w
+        self.h = h
+
+    def update(self, center):
+        self.center = center
+        self.topleft[0] = center[0] - self.h / 2
+        self.topleft[1] = center[1] - self.w / 2
+        self.bottomright[0] = center[0] + self.h / 2
+        self.bottomright[1] = center[1] + self.w / 2
+
+        return self
--- webcam.py
+++ webcam.py
@@ -2,10 +2,16 @@ import cv2
 import numpy as np
 import math
 import ball
+import rect
 
 cap = cv2.VideoCapture(0)
 
 pong = ball.ball(100,100, 10)
+
+center = [350,70]
+center2 = [350, 1000]
+paddle1 = rect.paddle(center, h = 200, w = 100)
+paddle2 = rect.paddle(center2, h = 200, w = 100)
 
 while(cap.isOpened()):
     ret, img = cap.read()
@@ -15,7 +21,7 @@ while(cap.isOpened()):
     cv2.rectangle(img,(1158,20),(1278,700),(0,255,0),0)
     crop_imgR = img[20:700, 1158:1278]
     cv2.circle(img, (pong.ypos, pong.xpos), 10, (250, 250, 250), thickness=20)
-    pong = ball.ball.update(pong, 0)
+
 
     # LEFT
     grey = cv2.cvtColor(crop_imgL, cv2.COLOR_BGR2GRAY)
@@ -114,15 +120,49 @@ while(cap.isOpened()):
 
 
     #Skin tone detector
-    # frame_threshed = cv2.inRange(hsv_img, SKINTONE_MIN, SKINTONE_MAX)
-    # imgray = frame_threshed
-    # ret,thresh = cv2.threshold(frame_threshed,127,255,0)
-    # contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    frame_threshed = cv2.inRange(hsv_img, SKINTONE_MIN, SKINTONE_MAX)
+    imgray = frame_threshed
+    ret,thresh = cv2.threshold(frame_threshed,127,255,0)
+    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-    # # Find the index of the largest contour
-    # areas = [cv2.contourArea(c) for c in contours]
-    # max_index = np.argmax(areas)
-    # cnt=contours[max_index]
+    # Find the index of the largest contour
+    areas = [cv2.contourArea(c) for c in contours]
+    max_index = np.argmax(areas)
+    cnt=contours[max_index]
+
+
+    count2 = 0;
+    yavg = 0;
+    for y in range(0, 700):
+        count = 0;
+        for x in range(20, 120):
+            if(frame_threshed[y][x] != 0): count += 1
+        if count >= 75:
+            yavg += y
+            count2 += 1
+
+    yavg = yavg/count2
+
+    count2 = 0;
+    yavg2 = 0;
+    for y in range(0, 700):
+        count = 0;
+        for x in range(700, 1300):
+            if (frame_threshed[y][x] != 0): count += 1
+        if count >= 75:
+            yavg += y
+            count2 += 1
+
+    yavg2 = yavg2 / count2
+
+
+
+    cv2.rectangle(img, paddle1.topleft, paddle1.bottomright, (0,255,0), thickness= -1, lineType=8, shift=0)
+    cv2.rectangle(img, paddle2.topleft, paddle2.bottomright, (0, 255, 0), thickness=-1, lineType=8, shift=0)
+
+    paddle1 = rect.paddle.update(paddle1, [yavg, 70])
+    paddle2 = rect.paddle.update(paddle2, [yavg2, 1000])
+    pong = ball.ball.update(pong, paddle1, paddle2)
 
     # x,y,w,h = cv2.boundingRect(cnt)
     # cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
